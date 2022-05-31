e349aacb352c8b1c7ab6594ad4bff87e7762adeb
Keaton <meek.keaton@gmail.com>
2022-05-30 23:41:59 -0500

    added the ability to return text from text

--- lib/main.dart
+++ lib/main.dart
@@ -24,69 +24,94 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MainApp(),
+      home: const MainPage(),
     );
   }
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+//main page of the webapp
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
 
   @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  String returnText = "";
+
+  //updates the text on the screen when the button is clicked
+  void updateText(String text) {
+    setState(() {
+      returnText = text;
+    });
+  }
+
+  //sets up all the functionality of the webpage
+  @override
   Widget build(BuildContext context) {
-    const appTitle = 'Anagram Application';
-    return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(appTitle),
-          centerTitle: true,
-        ),
-        body: const MyCustomForm(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Anagram Calculator'),
+      ),
+      body: Column(
+        children: <Widget>[
+          //displays the input box and the elevated button
+          //passes the text from the TextField and returns it to the webpage
+          AnagramInput(updateText),
+          Text(returnText),
+        ],
       ),
     );
   }
 }
 
-class MyCustomForm extends StatelessWidget {
-  const MyCustomForm({super.key});
+//displays the textfield and the elevated button for the user
+class AnagramInput extends StatefulWidget {
+  //creates function to return the value input
+  final Function(String) callback;
+  //passes the value to the updateText Function
+  AnagramInput(this.callback);
 
   @override
+  State<AnagramInput> createState() => _AnagramInputState();
+}
+
+class _AnagramInputState extends State<AnagramInput> {
+  //allows the button to retrieve the value of the text in the input field
+  TextEditingController inputController = TextEditingController();
+
+  //stores the value from the input field
+  late String getValue = "";
+
+  //cleanup resources after the value is stored from the string
+  @override
+  void dispose() {
+    super.dispose();
+    inputController.dispose();
+  }
+
+  //function that handles the click event to pass the value to the updateText function
+  void click() {
+    widget.callback(inputController.text);
+    inputController.clear();
+  }
+
+  //builds the textfield and the elevated button
+  @override
   Widget build(BuildContext context) {
-    /*return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      // ignore: prefer_const_literals_to_create_immutables
-      children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter a word to produce anagrams',
-            ),
-          ),
-        ),
-        ElevatedButton(
-          child: Text('Calculate'),
-          onPressed: () {
-            print("test");
-          },
-        ),
-      ],
-    );
-
-    */
-
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
           child: SizedBox(
             width: 500,
             child: TextField(
+              controller: inputController,
+              // ignore: prefer_const_constructors
               decoration: InputDecoration(
-                border: OutlineInputBorder(),
+                border: const OutlineInputBorder(),
                 hintText: 'Enter a word to produce anagrams',
               ),
             ),
@@ -101,11 +126,11 @@ class MyCustomForm extends StatelessWidget {
             ),
             child: const Text('Calculate'),
             onPressed: () {
-              // ignore: avoid_print
-              print("test");
+              //calls the click function to return the value to the screen
+              click();
             },
           ),
-        )
+        ),
       ],
     );
   }
