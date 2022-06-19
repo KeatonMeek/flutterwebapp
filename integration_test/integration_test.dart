0c768698d3c74eeaa58d578f9c4df4f2f11d05c4
Keaton <meek.keaton@gmail.com>
2022-06-19 17:34:37 -0500

    began building the actual integration test for the application

--- integration_test/integration_test.dart
+++ integration_test/integration_test.dart
@@ -1,15 +1,21 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:webapp/main.dart' as app;
+import 'robots/anagram_robot.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets("Tests expected return of anagram call",
+  testWidgets("Tests expected return of anagram call from REST Api",
       (WidgetTester tester) async {
     app.main();
 
-    await tester.pumpAndSettle();
+    AnagramRobot robot = AnagramRobot(tester);
+    //sleep(const Duration(seconds: 5));
+    await robot.inputTextField("Keaton");
+    sleep(const Duration(seconds: 5));
+    await robot.pressSubmit();
+    sleep(const Duration(seconds: 5));
   });
 }
